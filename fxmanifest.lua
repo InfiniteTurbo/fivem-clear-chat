f8bda61ef7dc7b6364dadacb262b0c27bc252851
Daniel <79483260+InfiniteTurbo@users.noreply.github.com>
2022-06-13 10:06:35 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version ‘adamant’
+game ‘gta5’
+
+
+title ‘FiveM Chat Clear’
+description ‘A simple FiveM script designed to help administrators clear the FiveM chat.’
+author ‘Turbo Modifications’
+
+
+server_script ‘server.lua’
